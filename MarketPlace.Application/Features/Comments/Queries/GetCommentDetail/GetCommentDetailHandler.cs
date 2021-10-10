8035a8d9799490bd591716f6e62c166c09ab8b2c
Z_Shobbar <zoha.shobbar@outlook.com>
2021-10-10 15:17:43 +0330

    add persistence project

--- MarketPlace.Application/Features/Comments/Queries/GetCommentDetail/GetCommentDetailHandler.cs
+++ MarketPlace.Application/Features/Comments/Queries/GetCommentDetail/GetCommentDetailHandler.cs
@@ -1,9 +1,9 @@
 ﻿using AutoMapper;
 using MarketPlace.Application.Contracts.Persistence;
+using MarketPlace.Application.DTOs;
 using MarketPlace.Domain.Entities;
 using MediatR;
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
